38188a721b16804f42a41469d9519125b4e9d047
andmorozov <bull13166@bk.ru>
2017-11-17 21:15:01 +0200

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,16 @@
-package ru.job4j;
-
+package chapter_001.src.main.java.ru.job4j;
+/**
+* Class Calculate решение задачи части 001 урок 1.1 "Hello world"
+*
+* @author Andrey Morozov (bull13166@bk.ru)
+* @version 1.0
+* @since 17.11.2017
+*/
 public class Calculate {
-	
+	/**
+	* Вывод строки на консоль
+
+	*/	
 	public static void main(String[] args) {
 		
 			System.out.println("Hello world");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Andrey Morozov (bull13166@bk.ru)
+* @version 1.0
+* @since 17.11.2017
+*/
+package ru.job4j;
