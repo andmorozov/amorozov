7e7bce08fe83acf78beb6bbf44a45e27fe579636
andmorozov <bull13166@bk.ru>
2017-11-18 00:58:18 +0200

    2.3. Элементарный калькулятор. [#185]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,52 @@
+package ru.job4j.calculator;
+/**
+* Class Calculator решение задачи части 001 урок 2.3 Элементарный калькулятор
+*
+* @author Andrey Morozov (bull13166@bk.ru)
+* @version 1.0
+* @since 18.11.2017
+*/
+public class Calculator {
+	/**
+	 * @param result результат
+	 */
+	private double result;
+	/**
+     * Сложение.
+	 * @param first первый аргумент
+	 * @param second второй аргумент
+	 */
+	public void add(double first, double second) {
+		this.result = first + second;
+	}
+	/**
+     * Вычитание.
+	 * @param first первый аргумент
+	 * @param second второй аргумент
+	 */
+	public void subtract(double first, double second) {
+		this.result = first - second;
+	}
+	/**
+     * Деление.
+	 * @param first первый аргумент
+	 * @param second второй аргумент
+	 */
+	public void div(double first, double second) {
+		this.result = first / second;
+	}
+	/**
+     * Умножение.
+	 * @param first первый аргумент
+	 * @param second второй аргумент
+	 */
+	public void multiple(double first, double second) {
+		this.result = first * second;
+	}
+	/**
+	 * @return результат
+	 */
+	public double getResult() {
+		return this.result;
+	}	
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+* Test.
+*
+* @author Andrey morozov (bull13166@bk.ru)
+* @version 1.0
+* @since 18.11.2017
+*/
+public class CalculatorTest {
+	/**
+	* Тест операции сложения.
+	*/ 
+    @Test
+    public void whenAddOnePlusOneThenTwo() {
+        Calculator calc = new Calculator();
+        calc.add(1D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+	/**
+	* Тест операции вычитания.
+	*/ 
+    @Test
+    public void whenSubtractTwoMinusOneThenOne() {
+        Calculator calc = new Calculator();
+        calc.subtract(2D, 1D);
+        double result = calc.getResult();
+        double expected = 1D;
+        assertThat(result, is(expected));
+    }
+	/**
+	* Тест операции деления.
+	*/ 
+    @Test
+    public void whenDivFourDivTwoThenTwo() {
+        Calculator calc = new Calculator();
+        calc.div(4D, 2D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+	/**
+	* Тест операции умножения.
+	*/ 
+    @Test
+    public void whenMultipleFourMultipleTwoThenEight() {
+        Calculator calc = new Calculator();
+        calc.multiple(4D, 2D);
+        double result = calc.getResult();
+        double expected = 8D;
+        assertThat(result, is(expected));
+    }
+}
